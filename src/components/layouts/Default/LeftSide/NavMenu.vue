30b6df069a2e0b7866999c17320f2f4603484e9f
sanmu <578595193@qq.com>
2023-05-10 17:14:14 +0800

    FIX: NavMenu Icon

--- src/components/layouts/Default/LeftSide/NavMenu.vue
+++ src/components/layouts/Default/LeftSide/NavMenu.vue
@@ -39,7 +39,10 @@ const onMenuItemClick = (route: any) => {
     <template v-for="menuRoute in menuRoutes" :key="menuRoute.name">
       <template v-if="menuRoute?.children?.length">
         <el-sub-menu :index="menuRoute.name">
-          <template #title>{{ menuRoute.meta.title }}</template>
+          <template #title>
+            <!-- <el-icon><i-ep-home-filled /></el-icon> -->
+            <span>{{ menuRoute.meta.title }}</span>
+          </template>
           <template v-for="subMenuRoute in menuRoute.children" :key="subMenuRoute.name">
             <el-menu-item :index="subMenuRoute.name" @click="onMenuItemClick(subMenuRoute)">{{
               subMenuRoute.meta?.title
@@ -48,9 +51,12 @@ const onMenuItemClick = (route: any) => {
         </el-sub-menu>
       </template>
       <template v-else>
-        <el-menu-item :index="menuRoute?.name" @click="onMenuItemClick(menuRoute)">{{
-          menuRoute?.meta.title
-        }}</el-menu-item>
+        <el-menu-item :index="menuRoute?.name" @click="onMenuItemClick(menuRoute)">
+          <!-- <el-icon><i-ep-home-filled /></el-icon> -->
+          <template #title>
+            {{ menuRoute?.meta.title }}
+          </template>
+        </el-menu-item>
       </template>
     </template>
   </el-menu>
@@ -59,8 +65,9 @@ const onMenuItemClick = (route: any) => {
 <style lang="scss" scoped>
 .el-menu {
   width: 250px;
+
   &.el-menu--collapse {
-    width: 100px;
+    width: 60px;
   }
 }
 </style>
